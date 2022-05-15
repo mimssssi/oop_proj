9003eb899c9e6dbdfa5e4468c71deac8e5b5298b
mimssssi <maria.rogleva@gmail.com>
2022-05-15 12:48:03 +0300

    added catch2, started working on library

--- oop_project1/Book.cpp
+++ oop_project1/Book.cpp
@@ -1,11 +1,11 @@
 #include "Book.hpp"
 
-unsigned Book::gen = 0;
+// unsigned Book::gen = 0;
 
 Book::Book(std::string author, std::string title, std::string path, std::string description, 
     unsigned rating, std::string ISBN) : 
     author(author), title(title), path(path), description(description), rating(rating), ISBN(ISBN)
    {
-       libID = gen++;
+    //    libID = gen++;
    }
 
